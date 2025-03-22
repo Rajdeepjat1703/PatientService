2a7bca1068f876b76dc3520b6603755b4849c270
Rajdeep Jat <rajdeepjat17@gmail.com>
2025-03-22 23:05:16 +0530

    first commit

---
+++ PatientSeviceApplication.java
@@ -0,0 +1,14 @@
+package com.example.Patient_sevice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+
+@SpringBootApplication
+public class PatientSeviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PatientSeviceApplication.class, args);
+	}
+
+}
---
+++ repository/PatientRepository.java
@@ -0,0 +1,15 @@
+package com.example.Patient_sevice.repository;
+
+import com.example.Patient_sevice.model.Patient;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface PatientRepository extends JpaRepository<Patient, UUID> {
+
+
+
+
+}
